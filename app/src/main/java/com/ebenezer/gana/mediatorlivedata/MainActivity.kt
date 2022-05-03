69b4efd761d316007c3be15967eac1e837969370
Gana Ebenezer <ebenezergana@gmail.com>
2022-05-03 08:27:34 +0100

    fixed app crash due to requesting for viewModel before onCreate

--- app/src/main/java/com/ebenezer/gana/mediatorlivedata/MainActivity.kt
+++ app/src/main/java/com/ebenezer/gana/mediatorlivedata/MainActivity.kt
@@ -1,21 +1,22 @@
 package com.ebenezer.gana.mediatorlivedata
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.ebenezer.gana.mediatorlivedata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
-    private val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
-        viewModel.currentScore.observe(this){score ->
+        viewModel.currentScore.observe(this) { score ->
             binding.previousPlayerScoreLabel.text = getString(
                 R.string.previous_player_score, score
             )
